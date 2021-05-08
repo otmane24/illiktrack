f42e48c9a2756987a48c0f6737eb84ae621eeced
Yacine <yacinobatlm13@gmail.com>
2021-05-08 17:40:28 +0100

    filtre json source

--- lib/Screen/home.dart
+++ lib/Screen/home.dart
@@ -7,52 +7,61 @@ import 'package:connectivity/connectivity.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:geocoding/geocoding.dart';
-import 'package:http/http.dart' as http ;
+import 'package:http/http.dart' as http;
 //import 'package:provider/provider.dart';
 
-class  Image {
+class Image {
+  final String imageName;
 
-  final  String imageName;
+  Image({this.imageName});
 
-  Image({ this.imageName});
-
-  factory Image.fromJson(Map<String, dynamic> parsedJson){
-    return Image(
-        imageName : parsedJson [ 'formatted_address' ]
-    );
+  factory Image.fromJson(Map<String, dynamic> parsedJson) {
+    return Image(imageName: parsedJson['formatted_address']);
   }
 }
+
 class Localisation {
   final List<Image> images;
 
   Localisation({this.images});
 
-  factory Localisation.fromJson(Map<String, dynamic> parsedJson){
-
-    var list = parsedJson [ 'results' ] as  List ;
+  factory Localisation.fromJson(Map<String, dynamic> parsedJson) {
+    var list = parsedJson['results'] as List;
     print('list : ${list.runtimeType}');
 
     List<Image> imagesList = list.map((i) => Image.fromJson(i)).toList();
 
-    return Localisation(
-        images : imagesList
-    );
+    return Localisation(images: imagesList);
   }
 }
 
-List<Localisation> analyseCars(String responseBody){
+List<Localisation> analyseCars(String responseBody) {
   final parsedJson = json.decode(responseBody);
-  return parsedJson<Localisation>((json) => Localisation.fromJson(json)).toList();
+  return parsedJson<Localisation>((json) => Localisation.fromJson(json))
+      .toList();
 }
-
+/*
 Future<List<Localisation>> fetchCars(Position place) async {
-  String url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=${place.latitude},${place.longitude}&key=AIzaSyBR046smIiQUeRbBaErZWkgtdiMFmOlAlc" ;
-  final  reponse = await http.get(Uri.parse(url));
+  String url =
+      "https://maps.googleapis.com/maps/api/geocode/json?latlng=${place.latitude},${place.longitude}&key=AIzaSyBR046smIiQUeRbBaErZWkgtdiMFmOlAlc";
+  final reponse = await http.get(Uri.parse(url));
   print("urlll : $url");
-  List <Localisation> resultat = analyseCars(reponse.body);
+  List<Localisation> resultat = analyseCars(reponse.body);
   return resultat;
 }
+*/
+//------------------------------------------
+
+Future<Map> getLocation(Position place) async {
+  String url =
+      "https://maps.googleapis.com/maps/api/geocode/json?latlng=34.881789,-1.316699&key=AIzaSyBR046smIiQUeRbBaErZWkgtdiMFmOlAlc";
+  http.Response response = await http.get(Uri.parse(url));
+  print(Uri.parse(url));
+  String source = response.body;
+  return json.decode(source);
+}
+
+//------------------------------------------
 
 class Home extends StatefulWidget {
   @override
@@ -69,9 +78,6 @@ class _HomeState extends State<Home> {
   Stream get connectionChange => connectionChangeController.stream;
   StreamController connectionChangeController = StreamController.broadcast();
 
-
-
-
   void _connectionChange(ConnectivityResult result) async {
     bool newStateConnection = await checkConnection();
     setState(() {
@@ -85,8 +91,18 @@ class _HomeState extends State<Home> {
       }
     }
   }
+  //----------------------
 
-  Future <bool> checkConnection() async {
+  void updateLocation(Position place) async {
+    Map locationMap = await getLocation(place);
+    setState(() {
+      _currentAddress = locationMap["results"][0]["formatted_address"];
+    });
+  }
+
+  //----------------------
+
+  Future<bool> checkConnection() async {
     bool previousConnection = hasConnection;
 
     try {
@@ -118,10 +134,9 @@ class _HomeState extends State<Home> {
   //final Geolocator geoLocator =Geolocator();
   Position _currentPosition;
   String _currentAddress;
-  Position position ;
+  Position position;
 
-
- // Stream <Position> positionStream =
+  // Stream <Position> positionStream =
 
   @override
   void dispose() async {
@@ -139,7 +154,6 @@ class _HomeState extends State<Home> {
   }
 
   Future _checkGps() async {
-    
     if (!(await Geolocator.isLocationServiceEnabled())) {
       if (Theme.of(context).platform == TargetPlatform.android ||
           Theme.of(context).platform == TargetPlatform.iOS) {
@@ -171,21 +185,19 @@ class _HomeState extends State<Home> {
     }
   }
 
-
-  Future <void> _getCurrentLocation() async{
-
-   await Geolocator.getCurrentPosition
-        (desiredAccuracy: LocationAccuracy.best)
+  Future<void> _getCurrentLocation() async {
+    await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
         .then((Position position) {
-      setState(() {
+      /* setState(() {
         _getAddressFromLatLng(position);
       });
-
+      */
     }).catchError((e) {
       print(e);
     });
   }
 
+/*
   _getAddressFromLatLng(Position currentPlace) async {
     try {
       List<Placemark> p = await placemarkFromCoordinates(
@@ -198,14 +210,14 @@ class _HomeState extends State<Home> {
             "${place.subLocality} ${place.locality} ${place.postalCode}" +
                 " ${place.country} ${place.isoCountryCode}";
       });
+
     } catch (e) {
       print(e);
     }
   }
-
+*/
   // ignore: cancel_subscriptions
-  Stream <Position> positionStreamSubcription = Geolocator.getPositionStream();
-
+  Stream<Position> positionStreamSubcription = Geolocator.getPositionStream();
 
   // ignore: close_sinks
 
@@ -213,96 +225,101 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return StreamBuilder(
       stream: positionStreamSubcription,
-      builder: (context ,snapShot){
-        if(snapShot.hasData) {
-          _currentPosition = snapShot.data ;
-          _getAddressFromLatLng(snapShot.data) ;
-          print(fetchCars(snapShot.data).toString()) ;
-        //print(_currentPosition == null ? 'Unknown' : _currentPosition.latitude.toString() + ', ' + _currentPosition.longitude.toString());
+      builder: (context, snapShot) {
+        if (snapShot.hasData) {
+          _currentPosition = snapShot.data;
+          updateLocation(snapShot.data);
+          //_getAddressFromLatLng(snapShot.data);
+          //print(fetchCars(snapShot.data).toString());
+          //print(_currentPosition == null ? 'Unknown' : _currentPosition.latitude.toString() + ', ' + _currentPosition.longitude.toString());
 
         }
         return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Illik'Track",
+          appBar: AppBar(
+            title: Text(
+              "Illik'Track",
+            ),
+            centerTitle: true,
           ),
-          centerTitle: true,
-        ),
-        body: hasConnection
-            ? SingleChildScrollView(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.all(8),
-                          child: Container(
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: <Widget>[
-                                  Icon(
-                                    Icons.location_on,
-                                    color: Colors.deepOrangeAccent,
-                                    size: 30,
-                                  ),
-                                  SizedBox(
-                                    width: 10,
-                                  ),
-                                  Expanded(
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                        SizedBox(
-                                          height: 5,
-                                        ),
-                                        Text((snapShot.hasData ? "lat : ${_currentPosition.latitude.toString()}  lon : ${_currentPosition.longitude.toString()}": "location" ),
-                                            style: TextStyle(
-                                              fontSize: 14,
-                                            )),
-                                        SizedBox(
-                                          height: 5,
-                                        ),
-                                        Container(
-                                          child: (_currentPosition != null &&
-                                                  _currentAddress != null)
-                                              ? Text(
-                                                  _currentAddress,
-                                                  style: TextStyle(
-                                                    fontSize: 18,
-                                                    color: Colors.tealAccent[200],
-                                                  ),
-                                                )
-                                              : Padding(
-                                                  padding: const EdgeInsets.only(
-                                                      top: 12.0, bottom: 2.0),
-                                                  child: LinearProgressIndicator(
-                                                    minHeight: 5,
-                                                  ),
-                                                ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  SizedBox(
-                                    width: 5,
-                                  ),
-                                ],
+          body: hasConnection
+              ? SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Container(
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                Icons.location_on,
+                                color: Colors.deepOrangeAccent,
+                                size: 30,
                               ),
-                            ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    Text(
+                                        (snapShot.hasData
+                                            ? "lat : ${_currentPosition.latitude.toString()}  lon : ${_currentPosition.longitude.toString()}"
+                                            : "location"),
+                                        style: TextStyle(
+                                          fontSize: 14,
+                                        )),
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    Container(
+                                      child: (_currentPosition != null &&
+                                              _currentAddress != null)
+                                          ? Text(
+                                              _currentAddress,
+                                              style: TextStyle(
+                                                fontSize: 18,
+                                                color: Colors.tealAccent[200],
+                                              ),
+                                            )
+                                          : Padding(
+                                              padding: const EdgeInsets.only(
+                                                  top: 12.0, bottom: 2.0),
+                                              child: LinearProgressIndicator(
+                                                minHeight: 5,
+                                              ),
+                                            ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                            ],
+                          ),
                         ),
-                      ],
-                    ),
-                  )
-            : Center(
-                child: Text(
-                  "Vérifiez votre internet",
-                  style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                )
+              : Center(
+                  child: Text(
+                    "Vérifiez votre internet",
+                    style: TextStyle(fontSize: 18),
+                  ),
                 ),
-              ),
-      );},
+        );
+      },
     );
   }
 }
